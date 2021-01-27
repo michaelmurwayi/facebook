ca278c59196ea16752fe8af727bb518ca0f8952c
michaelmurwayi <mikemurwayi7@gmail.com>
2021-01-27 07:18:32 +0300

    dashboard ui changes

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -39,7 +39,7 @@
                     <div class="avatar-preview">
                         <div id="imagePreview" style="background-image: url(require('../assets/logo.jpeg'));">
                         <img style="height:240px; width:100%; border-radius:100%; positon:absolute; top:0px;" :src="previewImage"/>
-                        <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg"  @change= "uploadImage" />
+                        <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg"  @change= "uploadImage($event)" />
                         </div>
                     </div>
                 </div>
@@ -87,25 +87,70 @@
         <div class="col-md-6 mt-5">
             <div class="card">
                 <div class="row">
-                    <div class="col-md-2 mt-2">
+                    <div class="col-md-2 mt-4">
 
                     <img class="rounded-circle" src="../assets/logo.jpeg" style="height:40px;">
-                     <!-- <input @change="GetImage" type="file" name="image" id=""> -->
                     </div>
-                    <div class=" mt-3">
-                        <form class="form-group" >
-                        <v-text-field placeholder="what's on your mind" ></v-text-field>
-                        </form>
+                    <div class=" col-md-6 mt-3 blue">
+                        <v-col
+                        cols="6"
+                        sm="6"
+                        v-model= "message"
+                        >
+                        <v-text-field class="mt-3"
+                            
+                        ></v-text-field>
+                        </v-col>
+                    </div>
+                    <div class="col-md-2 red mt-3">
+
+                    <button class="mt-3" @click="postMessage">Post</button>
                     </div>
                 </div>
                 <hr>
-                <div></div>
+                <div class="row mb-5" style="border-radius:25px 25px;">
+                    <div class="col-md-4">
+                        <button>Live videos</button>
+                    </div>
+                    <div class="col-md-4">
+                        <button>Photo/Video</button>
+                    </div>
+                    <div class="col-md-4">
+                        <button>Live Event</button>
+                    </div>
+                </div>
             </div>
+        <div class="col-md-12 mt-5" style="border-radius:15px 15px;">
             
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-2 mt-4">
+                        <h2>POST</h2>
+                    </div>
+                    <div class=" mt-3">
+                        <button class="btn btn-primary">Filter</button>
+                        <button class="btn btn-primary">Manage Posts</button>
+                    </div>
+                </div>
+                <hr>
+                <div class="row mb-5" style="border-radius:25px 25px;">
+                    <div class="col-md-6">
+                        <button>List View</button>
+                    </div>
+                    <div class="col-md-6">
+                        <button>Grid View</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+
+            </div>
+        </div>
+        
         </div>
         </div>
         
-    </div>
+   
 
 </v-app>
 </template>
@@ -182,7 +227,8 @@ li:hover{
 }
 </style>
 <script>
-import firebase from 'firebase/app'
+import Firebase  from 'firebase/app'
+import 'firebase/storage'
 require('firebase/auth')
 
     export default {
@@ -191,40 +237,77 @@ require('firebase/auth')
             return{
                previewImage:null,
                text:null,
+               message:null,
             }
         },
         methods:{
-            uploadImage(e){
-                const image = e.target.files[0];
+            uploadImage(event){
+                console.log(event.target.files);
+                const image = event.target.files[0];
                 const reader = new FileReader();
                 reader.readAsDataURL(image);
-                reader.onload = e =>{
-                    this.previewImage = e.target.result;
-                    console.log(this.previewImage);
-                    console.log(this.e.target.result);
-                    console.log("we are here");
+                reader.onload = event =>{
+                    this.previewImage = event.target.result;
+                    // console.log(this.previewImage);
+                    // console.log(image);
+
+                };
+                // Get a reference to the storage service, which is used to create references in your storage bucket
+                var storage = Firebase.storage();
+
+                // Create a storage reference from our storage service
+                var storageRef = storage.ref();
+                // Create the file metadata
+                var metadata = {
+                contentType: 'image/jpeg'
                 };
 
-            
+                // Upload file and metadata to the object 'images/mountains.jpg'
+                var uploadTask = storageRef.child(image.name).put(image, metadata);
+
+                // Listen for state changes, errors, and completion of the upload.
+                console.log(Firebase.storage.TaskEvent.STATE_CHANGED)
+                uploadTask.on(Firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+                (snapshot) => {
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                    case Firebase.storage.TaskState.PAUSED: // or 'paused'
+                        console.log('Upload is paused');
+                        break;
+                    case Firebase.storage.TaskState.RUNNING: // or 'running'
+                        console.log('Upload is running');
+                        break;
+                    }
+                }, () => {
+                    // Upload completed successfully, now we can get the download URL
+                    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                    console.log("we are here")
+                    console.log('File available at', downloadURL);
+                    });
+                }
+                );    
             },
+            
             logOut() {
             firebase.auth().signOut().then(() => {
                 firebase.auth().onAuthStateChanged(() => {
                 this.$router.push('/')
                 })
             })
-            }
-        //     savePost: function (e) {
-        //             e.preventDefault()
-        //             var message = this.text
-        //             // firebase.usersCollection.add({
-        //             //     message,
-        //             //     createdOn: firebase.firestore.Timestamp.now(),
-        //             //     updatedOn: firebase.firestore.Timestamp.now()
-        //             // })
+            },
+            postMessage: function (e) {
+                    e.preventDefault()
+                    var message = this.text
+                    // firebase.usersCollection.add({
+                    //     message,
+                    //     createdOn: firebase.firestore.Timestamp.now(),
+                    //     updatedOn: firebase.firestore.Timestamp.now()
+                    // })
 
-        //             console.log(firebase)
-        //         },
+                    console.log(firebase)
+                },
         }
      }  // missing closure added
 </script>
